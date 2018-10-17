8404e7ace408ba4bd3337f8effa7616f4109b0ae
matthew jones <matthewjones@cis.vvc.edu>
2018-10-16 20:05:54 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,43 @@
+/*
+Author: Matthew Jones.
+task study the string'
+created 10/16/18
+*/
+
+
 #include<iostream>
-
-
+ 
+#include<string> 
+ 
 using namespace std;
 
 
-int main()
-{
-    cout << "Hello World"<< endl;
 
-    return 0;
+int letter_a(string M)    
+{  
+	int count = 0;	
+	
+	for (int i =0; i < M.length(); i ++)
+		{
+		if (M.at(i) == 'a' || M.at(i) == 'A') 
+			{
+ 			count ++;   
+			} 
+		} 
+	return count;
+
 }
+
+	 	
+	int main() 
+{ 
+
+	string s = "abcABC"; 
+	cout << " enter your string " ;
+	getline(cin,s); 
+	cout << letter_a(s) << endl; 	
+return 0;  
+}		
+
+		
+			
